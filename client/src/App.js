13d5fe88d73e519e7b970147094f7b17568d59c9
kmiao <miaokaixiang@gmail.com>
2017-12-08 22:48:04 -0500

    Building basic skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import Cesium from 'cesium';
+//import Cesium from 'cesium/Build/Ceisum/Cesium.js';
 
 class App extends Component {
+  componentDidMount() {
+    console.log('Did Mount')
+  //  var viewer = new Cesium.Viewer('cesiumContainer');
+  }
+
   render() {
+//    console.log(Cesium);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-9">
+            <script src="../Build/Cesium/Cesium.js"></script>
+            <style> {"\
+              @import url(../Build/Cesium/Widgets/widgets.css);\
+              html, body, #cesiumContainer {\
+                width: 100%; height: 100%; margin: 0; padding: 0; overflow: hidden;\
+              }\
+            "}
+            </style>
+            <div id="cesiumContainer"></div>
+          </div>
+          <div className="col-sm-3">
+            Weather API
+          </div>
+        </div>
       </div>
     );
   }
