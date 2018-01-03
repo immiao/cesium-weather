14bc2ee92c8b9e0e60802db7d248e03e7424daef
kmiao <miaokaixiang@gmail.com>
2018-01-02 19:51:53 -0500

    Add page header

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,24 @@ class App extends Component {
   }
 
   render() {
+    var appStyle = {
+      marginLeft: "30px"
+    };
+
     return (
-      <div className="App">
-        <div className="col-sm-9">
-          <div id="cesiumContainer" ref={ element => this.cesiumContainer = element }/>
-        </div>
-        <div className="col-sm-3">
-          Hello
+      <div className="App" style={appStyle}>
+        <div className="row">
+            <div className="page-header">
+              <h1>Cesium Real Time Weather</h1>
+            </div>
+          <div className="span2">
+            <div className="col-sm-9">
+              <div id="cesiumContainer" ref={ element => this.cesiumContainer = element }/>
+            </div>
+            <div className="col-sm-3">
+              Hello
+            </div>
+          </div>
         </div>
       </div>
     );
