ca741e4570443ddbd2a513b7e1eecbd8a791bac1
kmiao <miaokaixiang@gmail.com>
2018-01-03 10:36:58 -0500

    Add weather info component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
+import WeatherInfo from "./WeatherInfo";
 
 class App extends Component {
   componentDidMount() {
@@ -14,15 +15,15 @@ class App extends Component {
     return (
       <div className="App" style={appStyle}>
         <div className="row">
-            <div className="page-header">
-              <h1>Cesium Real Time Weather</h1>
-            </div>
+          <div className="page-header">
+            <h1>Cesium Real Time Weather</h1>
+          </div>
           <div className="span2">
             <div className="col-sm-9">
               <div id="cesiumContainer" ref={ element => this.cesiumContainer = element }/>
             </div>
             <div className="col-sm-3">
-              Hello
+              <WeatherInfo/>
             </div>
           </div>
         </div>
---
+++ client/src/WeatherInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WeatherInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h3> City </h3>
+        <h5> Weather </h5>
+      </div>
+    );
+  }
+}
+
+export default WeatherInfo;
